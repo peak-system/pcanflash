peak-system/pcanflash

commit 885fa10af9086cf95812737f27d8ceac3c287a4b

pcanflash: add PCAN-MiniDiag FD flash target
Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

flash_types.h
@@ -107,5 +107,16 @@ const fblock_t flashid42[] = {
 	{0x800000, 0x800000, false}
 };
 
+const fblock_t flashid43[] = {
+	{0x00000, 0x8000, true},
+	{0x08000, 0x8000, true},
+	{0x10000, 0x8000, false},
+	{0x18000, 0x8000, false},
+	{0x20000, 0x8000, false},
+	{0x28000, 0x8000, false},
+	{0x30000, 0x8000, false},
+	{0x38000, 0x8000, false}
+};
+
 const fblock_t unknownflashid[] = {
 };

hardware_types.h
@@ -124,9 +124,9 @@ const hw_t hwt43 = {"PCAN-MiniDiag FD", "PCAN-MiniDiag-FD",
 		   0, /* CRC start */
 		   0, /* flash offset */
 		   256, /* max blocksize */
-		   UNKNOWN_FLASH_ID, /* Flash ID type */
-		   FLASH_BLOCK_ENTRIES(unknownflashid),
-		   unknownflashid};
+		   43, /* Flash ID type */
+		   FLASH_BLOCK_ENTRIES(flashid43),
+		   flashid43};
 
 const hw_t hwt44 = {"PCAN-MicroMod FD", "PCAN-MicroMod-FD",
 		   (FDATA_INVERT),

commit 223988e3b460d6f756033b1619383d65dff81a87

pcanflash: use Flash ID type 44 for PCAN-MiniDiag FD flash target
Fixes: 885fa10af908 ("pcanflash: add PCAN-MiniDiag FD flash target")
Signed-off-by: Oliver Hartkopp <socketcan@hartkopp.net>

flash_types.h
@@ -118,5 +118,24 @@ const fblock_t flashid43[] = {
 	{0x38000, 0x8000, false}
 };
 
+const fblock_t flashid44[] = {
+	{0x00000, 0x8000, true},
+	{0x08000, 0x8000, false},
+	{0x10000, 0x8000, false},
+	{0x18000, 0x8000, false},
+	{0x20000, 0x8000, false},
+	{0x28000, 0x8000, false},
+	{0x30000, 0x8000, false},
+	{0x38000, 0x8000, false},
+	{0x40000, 0x8000, false},
+	{0x48000, 0x8000, false},
+	{0x50000, 0x8000, false},
+	{0x58000, 0x8000, false},
+	{0x60000, 0x8000, false},
+	{0x68000, 0x8000, false},
+	{0x70000, 0x8000, false},
+	{0x78000, 0x8000, false}
+};
+
 const fblock_t unknownflashid[] = {
 };

hardware_types.h
@@ -130,14 +130,14 @@ const hw_t hwt42 = {"PCAN-Router Pro FD", "PCAN-Router_Pro_FD",
 		   flashid42};
 
 const hw_t hwt43 = {"PCAN-MiniDiag FD", "PCAN-MiniDiag-FD",
-		   (FDATA_INVERT | DATA_MODE8),
+		   (FDATA_INVERT | DATA_MODE8 | END_PROGRAMMING),
 		   0, /* CRC start */
 		   0, /* flash offset */
 		   0, /* file skip */
 		   256, /* max blocksize */
-		   43, /* Flash ID type */
-		   FLASH_BLOCK_ENTRIES(flashid43),
-		   flashid43};
+		   44, /* Flash ID type */
+		   FLASH_BLOCK_ENTRIES(flashid44),
+		   flashid44};
 
 const hw_t hwt44 = {"PCAN-MicroMod FD", "PCAN-MicroMod-FD",
 		   (FDATA_INVERT),